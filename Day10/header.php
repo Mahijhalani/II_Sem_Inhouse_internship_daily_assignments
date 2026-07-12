2930c9989c66e782d83d67b7ea6c10548c71a75c
Mahijhalani <mahijhalani15@gmail.com>
2026-07-12 11:24:14 -0700

    Create header.php

---
+++ Day10/header.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport"
+        content = "wit=dth=device-width, initial-scale=1.0">
+        <title>My Website</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <header class="bg-light border-bottom">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center py-3">
+                <img src="baddie.jpeg" alt ="photo" width="100">
+               
+    
+<div class="d-flex justify-content-center gap-3 mt-4">
+    <a href="home.php" class="btn btn-primary" onclick="window.location.href='home.php'">Home</a>
+    <a href="about.php" class="btn btn-primary" onclick="window.location.href='aboutus.php'">About</a>
+    <a href="contactus.php" class="btn btn-primary" onclick="window.location.href='contactus.php'">Contact Us</a>
+</div>
+
+</div>
+</div>
+</header>
+
+
+
