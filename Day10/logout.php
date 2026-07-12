020aaa905916607776bb0b1fac20dab4f72f2fd4
Mahijhalani <mahijhalani15@gmail.com>
2026-07-12 11:27:30 -0700

    Add logout.php for user logout functionality

---
+++ Day10/logout.php
@@ -0,0 +1,13 @@
+<?php
+include("header.php");
+?>
+
+<div class="container mt-5" style="max-width:400px;">
+    <form action="" method="post">
+       <h4>Logging out so early?<br>Everything okay?     </h4>
+        
+        
+        <button class="btn btn-primary w-100">Logout</button>
+</form>
+</div>
+
